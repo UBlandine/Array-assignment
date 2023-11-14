a13a699ad27152fe804ef0a2a5e162bfc97e4579
UBlandine <ugirablando@gmail.com>
2023-11-14 17:22:55 +0200

    Mine

---
+++ qs11.js
@@ -0,0 +1,14 @@
+//function that display pattern in triangular way
+
+function displayPattern(rows) {
+    for (let i = 0; i < rows; i++) {
+        let row = "";
+        for (let j = 0; j < rows - i; j++) {
+            row += "1 ";
+        }
+        console.log(row.trim());
+    }
+}
+
+// Example: Display the pattern
+displayPattern(4);
